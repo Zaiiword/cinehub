b9c99b502523dc33c1fd6ee67edaa835936d7922
Clement Dekeister <clement.dekeister.etu@univ-lille.fr>
2024-03-25 12:42:56 +0000

    init project

---
+++ src/main/java/fr/but3/cinehub/HelloWorld.java
@@ -0,0 +1,15 @@
+package fr.but3.cinehub;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+    
+    @GetMapping("/")
+    @ResponseBody
+    public String test(){; 
+        return "Hello World !";
+    }
+}
---
+++ src/test/java/fr/but3/cinehub/CinehubApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.but3.cinehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
