fe62f7d5997ce30e27b55a962aa53ed3ec88016c
Clement Dekeister <clement.dekeister.etu@univ-lille.fr>
2024-03-27 14:24:29 +0000

    add dependency

--- src/main/java/fr/but3/cinehub/HelloWorld.java
+++ src/main/java/fr/but3/cinehub/HelloWorld.java
@@ -2,14 +2,13 @@ package fr.but3.cinehub;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloWorld {
     
     @GetMapping("/")
-    @ResponseBody
-    public String test(){; 
-        return "Hello World !";
+    public String test(){;
+        return "test";
     }
 }
---
+++ src/main/java/fr/but3/cinehub/entity/Movie.java
@@ -0,0 +1,31 @@
+package fr.but3.cinehub.entity;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "movie")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Integer duration;
+    private Long rating;
+    private String directors;
+    private String actors;
+    private String synopsis;
+    private String trailer;
+    private Long poster;
+    private String genres;
+    private Date released;
+
+}
